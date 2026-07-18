aaa558a5b3cb5de177ac86134df85e99b0612062
JBenjamin01 <jmamanices@unsa.edu.pe>
2026-07-18 12:00:00 -0500

    Estructura general base del proyecto

---
+++ admin_linux/src/archivos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "archivos.h"
+#include "util.h"
+
+void menu_archivos(){
+
+    limpiar_pantalla();
+
+    printf("=====================================\n");
+    printf("       SHELL DE ARCHIVOS\n");
+    printf("=====================================\n\n");
+
+    printf("Modulo en desarrollo.\n");
+
+    pausar();
+
+}
---
+++ admin_linux/src/procesos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "procesos.h"
+#include "util.h"
+
+void menu_procesos(){
+
+    limpiar_pantalla();
+
+    printf("=====================================\n");
+    printf("   ADMINISTRADOR DE PROCESOS\n");
+    printf("=====================================\n\n");
+
+    printf("Modulo en desarrollo.\n");
+
+    pausar();
+
+}
