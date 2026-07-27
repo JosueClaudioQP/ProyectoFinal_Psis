3278703b80ef7b3d7882b52715be5fce4cb6c548
Gustavo <gcarrillov@unsa.edu.pe>
2026-07-26 23:24:52 -0500

    agregando modulo de descargas

--- include/descargas.h
+++ include/descargas.h
@@ -1,6 +1,32 @@
 #ifndef DESCARGAS_H
 #define DESCARGAS_H
 
+typedef struct nodo{
+
+    char origen[300];
+
+    char destino[300];
+
+    char nombre[150];
+
+    char estado[30];
+
+    struct nodo *sig;
+
+}nodo;
+
 void menu_descargas();
+
+void agregar_descarga();
+
+void mostrar_cola();
+
+void procesar_siguiente();
+
+void procesar_todas();
+
+void cancelar_descarga();
+
+void ver_historial_descargas();
 
 #endif
